a3fca3277ea92303fd054049e4d2ada26b60a94e
sanchez.horacio93@gmail.com <sanchez.horacio93@gmail.com>
2020-07-03 14:33:40 -0500

    Changing url of config

--- domain/config.py
+++ domain/config.py
@@ -6,9 +6,9 @@ import time
 """
 Constants that are used for script
 """
-url_data_town_hall = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=limite-de-las-alcaldias&q=&rows=16&facet=nomgeo"
+url_data_town_hall = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=limite-de-las-alcaldias&q=&rows=18&facet=nomgeo"
 
-url_data_units_location = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=prueba_fetchdata_metrobus&q="
+url_data_units_location = "https://datos.cdmx.gob.mx/api/records/1.0/search/?dataset=prueba_fetchdata_metrobus&rows=200&q="
 
 url_db = f"mysql+mysqlconnector://" + os.environ["DB_USERNAME"] + ":" + os.environ["DB_PASSWORD"] + \
          "@" + os.environ["DB_HOST"] + ":" + os.environ["DB_PORT"] + "/" + os.environ["DB_DATABASE"]
